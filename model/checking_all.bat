6cf6b22a2aa84aa7641f4c8d6b04671978567b87
gpu <wen_hongdiao@163.com>
2017-09-04 22:04:03 +0800

    new

---
+++ model/checking_all.bat
@@ -0,0 +1,7 @@
+python feature_generation.py
+python model_stage1.py
+python model_stage2.py
+python model_rnn.py
+python mlp.py
+python mlp_clarity.py
+python model_stage3.py
---
+++ model/checking_models.bat
@@ -0,0 +1,5 @@
+python model_stage2.py
+python model_rnn.py
+python mlp.py
+python mlp_clarity.py
+python model_stage3.py
